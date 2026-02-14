03e521f6c45d2301c71ea0ae92073a35207e9dad
Dyxux <thomasclotg@murena.io>
2026-02-14 01:32:41 +0100

    Item Group

--- src/main/java/com/github/seltademc/item/ModItemGroups.java
+++ src/main/java/com/github/seltademc/item/ModItemGroups.java
@@ -11,7 +11,17 @@ import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
 
-    public static final ItemGroup STICK_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP, Identifier.of(MoreVanillaVariants.MOD_ID, "stick_items"), FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.ACACIA_STICK)).displayName(Text.translatable("itemgroup.more-vanilla-variants.stick_items")).build());
+    public static final ItemGroup STICK_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP, Identifier.of(MoreVanillaVariants.MOD_ID, "stick_items"), FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.ACACIA_STICK)).displayName(Text.translatable("itemgroup.more-vanilla-variants.stick_items")).entries((displayContext, entries) -> {
+        entries.add(ModItems.ACACIA_STICK);
+        entries.add(ModItems.BIRCH_STICK);
+        entries.add(ModItems.CHERRY_STICK);
+        entries.add(ModItems.CRIMSON_STICK);
+        entries.add(ModItems.DARK_OAK_STICK);
+        entries.add(ModItems.JUNGLE_STICK);
+        entries.add(ModItems.MANGROVE_STICK);
+        entries.add(ModItems.SPRUCE_STICK);
+        entries.add(ModItems.WARPED_STICK);
+    }).build());
 
     public static void registerItemGroups() {
         MoreVanillaVariants.LOGGER.info("Registering Item Groups for " + MoreVanillaVariants.MOD_ID);
